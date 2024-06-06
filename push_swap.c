94cf7647855b3d36e2a96c1def6ee7ef52abf15c
ncollign <44343728+ncollign@users.noreply.github.com>
2024-06-06 14:11:31 +0200

    Update push_swap.c

--- push_swap.c
+++ push_swap.c
@@ -1,1 +1,15 @@
-test
+typedef struct s_stack
+{
+	int				value;
+	int				index;
+	struct s_list	*next;
+}				t_stack;
+
+int  main(int argc, char **argv)
+{
+  t_stack  **stack_a;
+	t_stack  **stack_b;
+
+  
+
+}
